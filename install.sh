37804924216bfd2fede1ae885284261f9de5a506
Josh Reichardt <josh.reichardt@gmail.com>
2019-12-08 09:45:20 -0800

    More tools

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 # Simple install script for various tools and configuration
 
-TOOLS="jq shellcheck fzf ripgrep hstr fd bat"
+TOOLS="jq shellcheck fzf ripgrep hstr fd bat yamllint"
+OSX_TOOLS="hadolint"
+LINUX_TOOLS=""
+PY_TOOLS="pylint pycodstyle"
+EXTRA_TOOLS="tflint tfsec"
 
 # Check is we are using osx, otherwise the Linux install is a little trickier
 if [[ "$(uname -s)" = "Darwin" ]]; then
@@ -17,7 +21,8 @@ fi
 
 # Set up our configs
 rm -rf ~/.zshrc || true
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/github.com/configs/josh.zsh-theme ~/.oh-my-zsh/themes/josh-custom.zsh-theme
 ln -s ~/github.com/configs/.zshrc ~/.zshrc
 ln -s ~/github.com/configs/.vimrc ~/.vimrc
