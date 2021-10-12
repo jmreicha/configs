65e411094d2f7d0706425734423c4011e06d4ca9
Josh Reichardt <josh.reichardt@gmail.com>
2021-10-12 21:41:07 +0000

    Fixes, better idempotency

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ install_nvm() {
     . ~/.nvm/nvm.sh
     nvm install --lts
     nvm alias default stable
-    npm install -g "$NODE_TOOLS"
+    npm install -g $NODE_TOOLS
 }
 
 configure() {
@@ -79,21 +79,24 @@ configure() {
     # powerlevel fonts
     curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf > "MesloLGS NF Regular.ttf"
 
-    # oh-my-zsh plugins
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
-    git clone --depth=1 https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-z
+    # oh-my-zsh
+    if [[ ! -d $HOME/.oh-my-zsh ]]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+        git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+        git clone --depth=1 https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-z
+    fi
 
     # Link configs
-    rm -rf ~/.oh-my-zsh/themese/josh-custom.zsh-theme ln -s ~/github.com/configs/josh.zsh-theme ~/.oh-my-zsh/themes/josh-custom.zsh-theme
+    rm -rf ~/.oh-my-zsh/themes/josh-custom.zsh-theme && ln -s ~/github.com/configs/josh.zsh-theme ~/.oh-my-zsh/themes/josh-custom.zsh-theme
     rm -rf ~/.zshrc && ln -s ~/github.com/configs/.zshrc ~/.zshrc
     rm -rf ~/.vimrc && ln -s ~/github.com/configs/.vimrc ~/.vimrc
     rm -rf ~/.p10k.zsh && ln -s ~/github.com/configs/.p10k.zsh ~/.p10k.zsh
 
     echo "Configuring Vim"
 
+    rm -rf ~/.vim/plugged/*
     # Vim 8.2+ tools/plugins + coc plugins
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -101,8 +104,17 @@ configure() {
     vim +'CocInstall coc-json coc-sh coc-yaml coc-go coc-pyright coc-go coc-docker coc-markdownlint' +qall
 }
 
+switch_shell() {
+    if [[ $SHELL != "/usr/bin/zsh" ]]; then
+        echo "Switching to zsh"
+        chsh -s $(which zsh)
+    fi
+}
+
 install_packages
 install_nvm
 configure
 # Change the shell as the last step because it is interactive
-chsh -s $(which zsh)
+switch_shell
+
+echo "Finished bootstrapping"
